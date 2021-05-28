11fa6ee87220f4ef2e69254a5e29d408e0a55fc6
v-king777 <v.kolpashchikov@gmail.com>
2021-05-28 23:23:38 +0300

    Добавьте файлы проекта.

---
+++ Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+                new Classroom { Students = { "Evgeniy", "Sergey", "Andrew" }, },
+                new Classroom { Students = { "Anna", "Viktor", "Vladimir" }, },
+                new Classroom { Students = { "Bulat", "Alex", "Galina" }, }
+            };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        private static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(a => a.Students).ToArray(); // Всего одна строка:) LINQ - это COOL!
+        }
+
+        public class Classroom
+        {
+            private List<string> students = new List<string>();
+
+            public List<string> Students { get => students; set => students = value; }
+        }
+    }
+}
